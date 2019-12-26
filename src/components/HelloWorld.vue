5c26662a62f03ee2832a8d83c664a904530fb87e
xiehailong6 <xiehailong6@jd.com>
2019-12-26 17:21:31 +0800

    213

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+	<p>{{name}}</p>
+	<button @click="changeName">按钮</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data(){
+	  return{
+		  name:"a"
+	  }
+  },
+  methods:{
+	  changeName(){
+	  }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
